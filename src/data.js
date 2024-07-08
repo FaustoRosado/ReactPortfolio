c18ee32d4c42532a883f89e26711ce0b363b6798
FaustoRosado <faustrosado@gmail.com>
2024-07-08 10:46:58 -0500

    practice build

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const projects = [
+  {
+    title: "ToDo List",
+    subtitle: "Using Javascript DOM Manipulation",
+    description:
+      "ToDo list using DOM manipulation to delete tasks using javascript functions",
+    image: "/images/todo_gif.gif",
+    link: "https://faustorosado.github.io/ToDoList/",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "images/logo192.png",
+    link: "https://reedbarger.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "images/logo512.png",
+    link: "https://jsbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "images/todolist.jpeg",
+    link: "https://pythonbootcamp.com",
+  },
+];
+
+export const skills = [
+
+];
+
+export const testimonials = [
+
+]
